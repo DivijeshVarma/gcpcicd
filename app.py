86c4a3ead1d3a6b63d87beabe20f63be1319afee
Divijeshvarma.a <divijeshvarma.a@pidatacenters.com>
2025-08-06 09:40:27 +0530

    approval changes

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Define a route for the root URL
 @app.route('/')
 def hello_world():
-    return 'Version 1'
+    return 'Version 1.0'
 
 # Run the Flask application if this file is executed directly
 if __name__ == '__main__':
